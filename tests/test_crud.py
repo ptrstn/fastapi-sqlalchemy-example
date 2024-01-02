402a8aaa02c7ca274714a7ee9809462ad529e204
Peter Stein <peterstein@protonmail.com>
2024-01-02 12:45:07 +0100

    Remove redundant comment

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,7 +1,7 @@
 from mypackage.crud import get_user_by_email
 
 
-def test_get_user_by_email(test_db):  # Pass the necessary fixtures as arguments
+def test_get_user_by_email(test_db):
     email = "non_existing@aol.com"
     db_user = get_user_by_email(test_db, email=email)
     assert db_user is None

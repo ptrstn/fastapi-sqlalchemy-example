3888d264944072e4bee799f6bca935eb1ee6f1e4
Peter Stein <peterstein@protonmail.com>
2024-11-20 20:15:13 +0100

    Fix DeprecationWarning on_event is deprecated
    DeprecationWarning:
    on_event is deprecated, use lifespan event handlers instead.
    
    Read more about it in the
    [FastAPI docs for Lifespan Events](https://fastapi.tiangolo.com/advanced/events/).

--- src/mypackage/main.py
+++ src/mypackage/main.py
@@ -1,14 +1,16 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from mypackage.api.v1.router import api_router
 from mypackage.database import create_db_and_tables
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-def on_startup():
+@asynccontextmanager
+async def lifespan(_: FastAPI):
     create_db_and_tables()
+    yield
 
 
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
